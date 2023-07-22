d24231abfd70e9a0d0a903248c8a0e36f84bf3db
Matheus Urban <matheusurban19@gmail.com>
2023-07-22 17:22:11 -0300

    calculadora

--- src/js/script.js
+++ src/js/script.js
@@ -1,3 +1,4 @@
-function somar(){
-    Array(num0.value || num1.value || num2.value || num3.value || num4.value || num5.value || num6.value || num7.value || num8.value || num9.value) + Array(num0.value || num1.value || num2.value || num3.value || num4.value || num5.value || num6.value || num7.value || num8.value || num9.value)
+const output = document.getElementById ("result") 
+function apagar(){
+    return --output
 }
