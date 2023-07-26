00194212a65ee0b335992e9c207d086202662129
Matheus Urban <matheusurban19@gmail.com>
2023-07-26 13:11:30 -0300

    Projeto Calculadora

--- src/js/script.js
+++ src/js/script.js
@@ -1,4 +1,13 @@
-const output = document.getElementById ("result") 
+const output = document.getElementById ("result")
+
+function operacao(){
+    const outputval = document.getElementById ("result").value
+    if(/^[0-9+\-*/.\s()]+$/.test(outputval)){
+        let resultado = eval(outputval);
+        return output.value = resultado;
+    }
+}
+
 function apagar(){
-    return --output
+    return output.value = ' ';
 }
